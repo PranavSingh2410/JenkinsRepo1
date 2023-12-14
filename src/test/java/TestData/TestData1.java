b1768a80af002fe648d10638b23d2e13c0238dd2
PranavSingh2410 <pranavks223@gmail.com>
2023-12-14 13:04:11 +0530

    1st commit

---
+++ src/test/java/TestData/TestData1.java
@@ -0,0 +1,15 @@
+package TestData;
+
+import org.testng.annotations.Test;
+
+public class TestData1 {
+
+	@Test
+	public void M1() 
+	{
+		System.out.println("jenkins practice");
+		
+		System.out.println("jenkins executions");
+	}
+
+}
